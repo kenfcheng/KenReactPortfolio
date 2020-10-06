66c244f7b69c85ecd6ca83b650432c65d911b912
Ken Cheng <kenfcheng@gmail.com>
2020-10-06 10:26:26 -0400

    integrated nabar into header

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -4,9 +4,28 @@ const Header = () => {
   return (
     <div>
       <section>
-        <h1 className="navbar navbar-home display-6 bg-secondary text-white mr-">
-          Portfolio
-        </h1>
+        <nav className="navbar navbar-light"></nav>
+      </section>
+      <h1 className="navbar navbar-home display-6 bg-secondary text-white mr-">
+        Portfolio
+      </h1>
+      <section className="navbar-nav ml-auto">
+        <div className="navbar-item">
+          <a href="contact.html" className="navbar-link">
+            <button type="button" className="btn btn-info">
+              Contact Me
+            </button>
+          </a>
+        </div>
+      </section>
+      <section className="navbar-nav">
+        <div className="navbar-item">
+          <a href="index.html" className="navbar-link">
+            <button type="button" className="btn btn-info">
+              About Me
+            </button>
+          </a>
+        </div>
       </section>
     </div>
   );
