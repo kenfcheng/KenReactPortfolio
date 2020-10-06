aeecf7c2fe726bd86fd2015714d7e88b28d57b47
Ken Cheng <kenfcheng@gmail.com>
2020-10-06 10:00:24 -0400

    Header created

---
+++ portfolio/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <section>
+        <h1 className="navbar navbar-home display-6 bg-secondary text-white mr-">
+          Portfolio
+        </h1>
+      </section>
+    </div>
+  );
+};
+
+export default Header;
