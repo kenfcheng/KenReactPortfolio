c10950263bb0ba5ec4bf1cd894e784061f27f75d
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2020-11-11 19:29:10 -0500

    working on carousel in contacts

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 // import ReactDOM from ('react-dom');
+
 import LinkedIn from "./LinkedIn";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Parallax, Background } from "react-parallax";
 import constellation from "../Images/constellationbackground.jpg.jpg";
+import gitHubLogo from "../Images/GitHubLogo.png";
 import { Carousel } from "react-responsive-carousel";
 
 class Contact extends Component {
@@ -27,9 +29,26 @@ class Contact extends Component {
             <LinkedIn />
           </div>
 
-          <div>
-            <img src="assets/2.jpeg" />
-            <p className="legend">Legend 2</p>
+          <div
+            style={{
+              backgroundImage: `url(${constellation})`,
+              backgroundSize: "cover",
+              width: 799,
+              height: 400,
+            }}
+          >
+            <div
+              className="card s4 push-s5"
+              style={{ width: 350, height: 263 }}
+            >
+              <a href="https://github.com/kenfcheng">
+                <i
+                  src={gitHubLogo}
+                  alt="GitHub"
+                  style={{ width: 150, height: 113 }}
+                ></i>
+              </a>
+            </div>
           </div>
           <div>
             <img src="assets/3.jpeg" />
