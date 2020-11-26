3e91bd58006d79d59f5d93c15b2b6ded18c0664f
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2020-11-26 11:47:10 -0500

    model modifying

--- src/Components/demoModal.js
+++ src/Components/demoModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import Player from "./reactplayer";
+import "../Styles/modal.css";
 
 Modal.setAppElement("#root");
 
@@ -21,9 +22,14 @@ export default function DemoModal() {
         isOpen={isOpen}
         onRequestClose={toggleModal}
         contentLabel="My dialog"
+        className="grey lighten-1"
       >
         <Player />
-        <button onClick={toggleModal}>Close</button>
+        <br></br>
+        <br></br>
+        <button className="btn s12 m4" onClick={toggleModal}>
+          Close
+        </button>
       </Modal>
     </div>
   );
