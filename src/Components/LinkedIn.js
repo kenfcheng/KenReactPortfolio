9d3923370b5fcc807a97e3e8fbb96416015b9eae
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-01-22 17:44:47 -0500

    LinkedIn

--- src/Components/LinkedIn.js
+++ src/Components/LinkedIn.js
@@ -23,8 +23,6 @@ const LinkedIn = () => {
       <h6 className="col 8 push-s2 white-text">
         Phone Number: +1 704-692-7685
       </h6>
-
-      <i className="fab fa-github"></i>
     </div>
   );
 };
